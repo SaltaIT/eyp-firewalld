d9bf65cd47e4e56c5a2f94cf2ee1c6a821433577
Jordi Prats <jprats@systemadmin.es>
2016-07-15 17:43:13 +0200

    add port firewall-cmd

--- manifests/addport.pp
+++ manifests/addport.pp
@@ -3,6 +3,21 @@ define firewalld::addport (
                             $permanent = true,
                             $port      = $name,
                             $zone      = 'public',
+                            $protocol  = 'tcp',
                           ) {
+  Exec {
+    path => '/usr/sbin:/usr/bin:/sbin:/bin',
+  }
+
+  if(!defined(Service['firewalld']))
+  {
+    fail('firewalld undefined, please check firewalld class and firewalld::ensure')
+  }
+
+  # command => "firewall-cmd --permanent --zone=public --add-port=5666/tcp",
+  exec { "firewalld add port ${port} ${name} ${zone}":
+    command => inline_template('firewall-cmd <% if @permanent %>--permanent<% end %> --zone=<%= @zone %> --add-port=<%= @port %>/<%= @protocol %>'),
+    notify => Service['firewalld'],
+  }
 
 }
