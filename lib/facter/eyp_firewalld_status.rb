4e810257424c0352ef43a438b78aa9be74e4633a
Jordi Prats <jordi.prats@gmail.com>
2018-04-05 18:35:29 +0200

    bugfix

--- lib/facter/eyp_firewalld_status.rb
+++ lib/facter/eyp_firewalld_status.rb
@@ -1,6 +1,6 @@
 retcode = Facter::Util::Resolution.exec('bash -c \'/usr/bin/systemctl status firewalld >/dev/null 2>&1; echo $?\'').to_s
 
-unless env.nil? or env.empty?
+unless retcode.nil? or retcode.empty?
   Facter.add('eyp_firewalld_status') do
       setcode do
         retcode
